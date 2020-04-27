35fe03b09753abe1f20fa66307aa43b82daa47cb
David-Kuku <psalmsgus99@gmail.com>
2020-04-27 09:52:35 +0100

    addiing project

---
+++ js/alpha.js
@@ -0,0 +1,8 @@
+function openForm() {
+  document.getElementById("myform").style.display = "block";
+  document.getElementById("bab").style.display="none";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
